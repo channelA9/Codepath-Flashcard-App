bd9fdee5aef97478379430b061be8ab065e039df
channel9a <shaun.colegado@gmail.com>
2022-10-03 23:13:13 -0700

    added optional functionalities

---
+++ app/src/main/java/com/example/codepathflashcards/AddCardActivity.kt
@@ -0,0 +1,75 @@
+package com.example.codepathflashcards
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+
+class AddCardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_card)
+
+        val backButton = findViewById<ImageView>(R.id.backButton)
+        val saveButton = findViewById<ImageView>(R.id.saveButton)
+        val questionText = findViewById<EditText>(R.id.questionText)
+        val answerText = findViewById<EditText>(R.id.answerText)
+        val w1AnswerText = findViewById<EditText>(R.id.wrong1Text)
+        val w2AnswerText = findViewById<EditText>(R.id.wrong2Text)
+
+        // importing from intent
+
+        val originalA =  intent.getStringExtra("ORIGINAL_ANSWER_KEY")
+        val originalQ = intent.getStringExtra("ORIGINAL_QUESTION_KEY")
+
+
+        questionText.setText(originalQ)
+        answerText.setText(originalA)
+
+        backButton.setOnClickListener {
+            finish()
+        }
+
+        saveButton.setOnClickListener {
+
+            val data = Intent() // create a new Intent, this is where we will put our data
+            val qString = questionText.text.toString()
+            val aString = answerText.text.toString()
+            val w1String = w1AnswerText.text.toString()
+            val w2String = w2AnswerText.text.toString()
+
+            if (qString.isNotEmpty()) {
+                data.putExtra(
+                    "QUESTION_KEY",
+                    qString
+                )
+
+                data.putExtra(
+                    "ANSWER_KEY",
+                    aString
+                )
+
+                data.putExtra(
+                    "WRONG_1_ANSWER_KEY",
+                    w1String
+                )
+
+                data.putExtra(
+                    "WRONG_2_ANSWER_KEY",
+                    w2String
+                )
+
+                setResult(RESULT_OK, data)
+
+                Toast.makeText(applicationContext, "Successfully changed card!", Toast.LENGTH_SHORT).show();
+
+                finish()
+            }
+            else {
+                Toast.makeText(applicationContext, "Please fill in all the boxes!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/codepathflashcards/MainActivity.kt
+++ app/src/main/java/com/example/codepathflashcards/MainActivity.kt
@@ -1,24 +1,51 @@
 package com.example.codepathflashcards
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import android.widget.RelativeLayout
 import android.widget.TextView
 import android.widget.ToggleButton
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
         val flashQst = findViewById<TextView>(R.id.flashcard_question)
         val flashAns = findViewById<TextView>(R.id.flashcard_answer)
         var flashSelection = findViewById<RelativeLayout>(R.id.answers)
         val flashA1 = findViewById<TextView>(R.id.answer_1)
         val flashA2 = findViewById<TextView>(R.id.answer_2)
         val flashA3 = findViewById<TextView>(R.id.answer_3)
-
+        val addBtn = findViewById<ImageView>(R.id.addButton)
+        val editBtn = findViewById<ImageView>(R.id.editButton)
         val toggle = findViewById<ToggleButton>(R.id.toggleAns)
+
+
+        val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            val data: Intent? = result.data
+            if (data != null) {
+                val qString = data.getStringExtra("QUESTION_KEY")
+                val aString = data.getStringExtra("ANSWER_KEY")
+                val w1String  = data.getStringExtra("WRONG_1_ANSWER_KEY")
+                val w2String = data.getStringExtra("WRONG_2_ANSWER_KEY")
+                flashQst.text = qString
+                flashAns.text = aString
+
+                var arrayOfAnswers = arrayOf(aString, w1String, w2String)
+
+                arrayOfAnswers.shuffle()
+
+                flashA1.text = arrayOfAnswers[0]
+                flashA2.text = arrayOfAnswers[1]
+                flashA3.text = arrayOfAnswers[2]
+            }
+        }
 
         var showAnswers = true
         var answered = false
@@ -84,6 +111,18 @@ class MainActivity : AppCompatActivity() {
                 showAnswers = false
                 flashSelection.visibility = View.GONE
             }
+        }
+
+        addBtn.setOnClickListener {
+            val intent = Intent(this, AddCardActivity::class.java)
+            resultLauncher.launch(intent)
+        }
+
+        editBtn.setOnClickListener {
+            val intent = Intent(this, AddCardActivity::class.java)
+            intent.putExtra("ORIGINAL_QUESTION_KEY", flashQst.text.toString());
+            intent.putExtra("ORIGINAL_ANSWER_KEY", flashAns.text.toString());
+            resultLauncher.launch(intent)
         }
 
     }
